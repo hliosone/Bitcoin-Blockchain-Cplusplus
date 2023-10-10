961cce2771535b59a81e1e4cf5dad6a4eee5c919
hliosone <sstelcher@protonmail.com>
2023-10-10 21:25:35 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+class Block {
+
+public:
+    Block(string _timestamp,unsigned long _lasthash,unsigned long _hash, string _data) : timestamp(_timestamp),
+    lastHash(_lasthash), hash(_hash), data(_data){};
+    string blockString() const{
+        return "Block : \nTimestamp : " + timestamp + "\nLast Hash : " + to_string(lastHash) +
+        "\nHash : " + to_string(hash) + "\nData : " + data;
+    }
+
+    Block static genesis(){
+        return Block("date",0, rand() % std::numeric_limits<unsigned long>::max(), "Genesis block");
+    }
+
+    string static getCurrentTime(){
+        auto currentTime = ::chrono::system_clock::to_time_t(chrono::system_clock::now());
+        return (string) std::ctime(&currentTime);
+    }
+
+private :
+    const string timestamp;
+    const unsigned long lastHash;
+    const unsigned long hash;
+    const string data = "";
+};
+
+int main() {
+    srand(time(0));
+    Block one("test",321, 213, "mes txs");
+    cout << one.blockString() << endl;
+//
+//    Block test = Block::genesis();
+
+    return 0;
+}
